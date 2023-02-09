bebd4608f7b794ba3700aea1a1d0a6f60152c4dc
yurasava <yurasava@gmail.com>
2023-02-09 20:56:17 +0300

    TGBOT-291 created Car record

---
+++ web-core/src/main/java/com/tutrit/bean/Car.java
@@ -0,0 +1,13 @@
+package com.tutrit.bean;
+
+public record Car(String id,
+                  String owner,
+                  String vin,
+                  String plateNumber,
+                  String brand,
+                  String model,
+                  String generation,
+                  String modification,
+                  String engine,
+                  int year) {
+}
