391111a56ea0573fdc6599b71d536cafdcfb8723
yurasava <yurasava@gmail.com>
2023-02-10 18:44:59 +0300

    TGBOT-295 added methods to CarGateway

--- web-ui/src/main/java/com/tutrit/gateway/CarGateway.java
+++ web-ui/src/main/java/com/tutrit/gateway/CarGateway.java
@@ -1,4 +1,17 @@
 package com.tutrit.gateway;
 
+import com.tutrit.bean.Car;
+import org.springframework.stereotype.Component;
+
+import java.net.http.HttpClient;
+
+@Component
 public interface CarGateway {
+    HttpClient httpClient = null;
+
+    Car getCar();
+    String saveCar();
+    String updateCar();
+    String changeCar();
+    boolean deleteCar();
 }
