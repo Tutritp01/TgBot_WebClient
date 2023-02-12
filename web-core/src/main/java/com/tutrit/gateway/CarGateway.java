1ed518aec933042bf78965d0907c199c9eeeeaef
yurasava <yurasava@gmail.com>
2023-02-12 20:50:35 +0300

    TGBOT-295 update CarGateway

--- web-core/src/main/java/com/tutrit/gateway/CarGateway.java
+++ web-core/src/main/java/com/tutrit/gateway/CarGateway.java
@@ -3,12 +3,8 @@ package com.tutrit.gateway;
 import com.tutrit.bean.Car;
 import org.springframework.stereotype.Component;
 
-import java.net.http.HttpClient;
-
 @Component
 public interface CarGateway {
-    HttpClient httpClient = null;
-
     Car getCar();
     String saveCar();
     String updateCar();
