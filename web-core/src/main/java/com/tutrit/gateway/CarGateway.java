f1c2ce609e21f8b889119057a4571152dfdb40c2
yurasava <yurasava@gmail.com>
2023-02-14 17:45:49 +0300

    TGBOT-291 added methods to CarGateway

--- web-core/src/main/java/com/tutrit/gateway/CarGateway.java
+++ web-core/src/main/java/com/tutrit/gateway/CarGateway.java
@@ -1,4 +1,9 @@
 package com.tutrit.gateway;
 
+import com.tutrit.bean.Car;
+
 public interface CarGateway {
+    Car findCarById(String id);
+    Car saveCar(Car car);
+    boolean deleteCar(String id);
 }
