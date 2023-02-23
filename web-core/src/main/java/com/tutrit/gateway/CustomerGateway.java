0dcdb1e76416f6b7ad9eb325ffef4717d3d1e19f
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-02-23 19:52:50 +0300

    Change Optional<Customer> on the CustomerGateway

--- web-core/src/main/java/com/tutrit/gateway/CustomerGateway.java
+++ web-core/src/main/java/com/tutrit/gateway/CustomerGateway.java
@@ -5,7 +5,7 @@ import com.tutrit.bean.Customer;
 import java.util.Optional;
 
 public interface CustomerGateway {
-    Optional<Customer> saveCustomer (Customer customer);
+    Customer saveCustomer(Customer customer);
     Optional<Customer> findCustomerById(String customerId);
     boolean deleteCustomerById(String customerId);
 }
