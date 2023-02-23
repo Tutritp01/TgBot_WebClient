d38deac4217fd24e0a91a06aa8b1a9ec9480e44b
yurasava <yurasava@gmail.com>
2023-02-23 12:36:37 +0300

    TGBOT-291 delete redundant methods from car gateway

--- web-core/src/main/java/com/tutrit/gateway/CarGateway.java
+++ web-core/src/main/java/com/tutrit/gateway/CarGateway.java
@@ -7,8 +7,4 @@ import java.util.Optional;
 public interface CarGateway {
     Optional<Car> saveCar(Car car);
     Optional<Car> findCarById(String id);
-    Iterable<Car> findAllCars();
-    Optional<Car> update(String id);
-    boolean deleteCar(Car car);
-    boolean deleteCarById(String id);
 }
