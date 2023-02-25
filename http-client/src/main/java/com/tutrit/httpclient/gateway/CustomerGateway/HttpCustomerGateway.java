4d5163bf28668776251e2ee3a9c8e7d8f9c3c300
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-02-26 01:39:31 +0300

    Add new implements method and code refactoring

--- http-client/src/main/java/com/tutrit/httpclient/gateway/CustomerGateway/HttpCustomerGateway.java
+++ http-client/src/main/java/com/tutrit/httpclient/gateway/CustomerGateway/HttpCustomerGateway.java
@@ -1,6 +1,7 @@
 package com.tutrit.httpclient.gateway.CustomerGateway;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tutrit.bean.Customer;
 import com.tutrit.gateway.CustomerGateway;
@@ -16,45 +17,59 @@ import java.util.Optional;
 @Component
 public class HttpCustomerGateway implements CustomerGateway {
     @Override
-    public Customer saveCustomer(Customer customer) throws IOException, InterruptedException {
+    public Customer saveCustomer(Customer customer){
         HttpClient httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/customers"))
-                .POST(HttpRequest.BodyPublishers.noBody())
-
+                .uri(URI.create("http://localhost:8080/customersData")) //endpoint to transfer data
+                .POST(HttpRequest.BodyPublishers
+                        .ofString(String.format(
+                                "{\"customerId\":\"%s\"\"name\":\"%s\",\"city\":\"%s\",\"phoneNumber\":\"%s\",\"email\":\"%s\"}",
+                                customer.customerId(),customer.name(),customer.city(),customer.phoneNumber(),customer.email())))
                 .header("Content-Type", "application/json")
                 .build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         // print status code
         System.out.println(response.statusCode());
         return customer;
     }
 
-
     @Override
-    public Optional<Customer> findCustomerById(String customerId) throws IOException, InterruptedException {
+    public Optional<Customer> findCustomerById(String customerId){
         HttpClient httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
 
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
-                .uri(URI.create("http://localhost:8080/customers"))
+                .uri(URI.create("http://localhost:8080/customersDataBase")) //endpoint to find customer
                 .build();
 
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = null;
+        try {
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         Customer customer = null;
         if (response.request().bodyPublisher().isPresent()) {
             ObjectMapper objectMapper = new ObjectMapper();
-            customer = objectMapper.readValue(response.body(), Customer.class);
+            try {
+                customer = objectMapper.readValue(response.body(), Customer.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
         }
-
 
         return Optional.ofNullable(customer);
     }
@@ -62,6 +77,11 @@ public class HttpCustomerGateway implements CustomerGateway {
     @Override
     public boolean deleteCustomerById(String customerId) {
         return false;
+    }
+
+    @Override
+    public Optional<Customer> updateCustomer(String customerId) {
+        return Optional.empty();
     }
 
 
