413ea298f9dce07523caf1f3973c70c109cfd28f
Sav4ykVladzislau <106885014+Sav4ykVladzislau@users.noreply.github.com>
2023-02-26 12:57:32 +0300

    [TGBOT-289] Customer methods (#8)
    * Create bean Customer
    
    * Create HTML customer-form
    
    * Create CustomerGateway and CustomerController
    
    * Update Customer.java
    
    * Update customer-form.html
    
    * change customer-form.html
    
    * Change directory
    
    * Change customer-form.html
    
    * Create mapper and spring context class
    
    * Create test for userController
    
    * Change pom file
    
    * Change file
    
    * Change dependency from pom.xml
    
    * Moved to the correct directory
    and create test for save and delete method
    
    * add method deleteCustomerById
    
    * change customer-form.html
    
    * Update customer-form.html
    
    add empty line
    
    * Change file name by SpringContext
    
    * Change returned object in CustomerGateway Customer -> Optional
    
    * Change returned object in CustomerGateway Customer -> Optional
    and change test
    
    * Change returned object in CustomerGateway Customer -> Optional
    and change test
    
    * Change customers-form.html to customer-form & remove HttpServletRequest r row
    
    * Change Optional<Customer> on the CustomerGateway
    
    * Change input on the post method
    
    * Create new endpoint /customer and create test
    
    * Delete endpoint /customer and test
    
    * Add error message in customer-form.html
    
    * Moved "spring-boot-starter-web" dependency in the child pom

---
+++ web-core/src/main/java/com/tutrit/gateway/CustomerGateway.java
@@ -0,0 +1,11 @@
+package com.tutrit.gateway;
+
+import com.tutrit.bean.Customer;
+
+import java.util.Optional;
+
+public interface CustomerGateway {
+    Customer saveCustomer(Customer customer);
+    Optional<Customer> findCustomerById(String customerId);
+    boolean deleteCustomerById(String customerId);
+}
