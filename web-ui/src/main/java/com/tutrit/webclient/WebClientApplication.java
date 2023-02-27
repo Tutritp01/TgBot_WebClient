9f5e0a99b803e0b91c059136067ecd9d082b75f9
yurasava <yurasava@gmail.com>
2023-02-27 21:48:16 +0300

    TGBOT-295 added component scan to main class

--- web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
+++ web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
@@ -2,8 +2,10 @@ package com.tutrit.webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tutrit")
 public class WebClientApplication {
 
 	public static void main(String[] args) {
