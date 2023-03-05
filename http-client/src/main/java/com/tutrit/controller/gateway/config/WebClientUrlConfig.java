cf328275b2e9c0e127e9563e3f24292f934691c9
yurasava <yurasava@gmail.com>
2023-03-05 10:42:28 +0300

    TGBOT-295 added WebClientUrlConfig

---
+++ http-client/src/main/java/com/tutrit/controller/gateway/config/WebClientUrlConfig.java
@@ -0,0 +1,29 @@
+package com.tutrit.controller.gateway.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+
+@Configuration
+public class WebClientUrlConfig {
+
+    @Value("${endpoint.web-client}")
+    private String webClientUrl;
+
+    String getUrl() {
+        return webClientUrl;
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
