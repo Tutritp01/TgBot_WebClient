8ee886d397ccd62235af1d204f7e4f5ce6a7e227
yurasava <yurasava@gmail.com>
2023-03-07 21:20:51 +0300

    TGBOT-295 add json ignore properties

--- web-core/src/main/java/com/tutrit/bean/Car.java
+++ web-core/src/main/java/com/tutrit/bean/Car.java
@@ -1,7 +1,9 @@
 package com.tutrit.bean;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.soabase.recordbuilder.core.RecordBuilder;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @RecordBuilder
 public record Car(String carId,
                   String owner,
