37b1c022dd92bd57a308b2dbcd970e56ffcc3f65
yurasava <yurasava@gmail.com>
2023-03-08 21:36:35 +0300

    TGBOT-295 remove component scan

--- web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
+++ web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
@@ -2,10 +2,8 @@ package com.tutrit.webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.tutrit")
 public class WebClientApplication {
 
     public static void main(String[] args) {
