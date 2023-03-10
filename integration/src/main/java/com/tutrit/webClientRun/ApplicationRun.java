552cc2e67000494644df041fe7339478a0fa8229
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-03-11 00:20:02 +0300

    move pom file by new package

--- integration/src/main/java/com/tutrit/webClientRun/ApplicationRun.java
+++ integration/src/main/java/com/tutrit/webClientRun/ApplicationRun.java
@@ -1,9 +1,11 @@
-package com.tutrit;
+package com.tutrit.webClientRun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages  ="com.tutrit")
 public class ApplicationRun {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRun.class, args);
--- integration/src/test/java/com/tutrit/ApplicationRunTest.java
+++ integration/src/test/java/com/tutrit/ApplicationRunTest.java
@@ -1,6 +1,7 @@
 package com.tutrit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutrit.webClientRun.ApplicationRun;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
