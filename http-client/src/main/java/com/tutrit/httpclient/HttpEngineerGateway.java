2fc4ff657be298b0c0bdce8120aa4c4daf5d583e
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2023-03-11 18:23:37 +0300

    [TGBOT-297] Create HttpClient EngineerGateway implementation

---
+++ http-client/src/main/java/com/tutrit/httpclient/HttpEngineerGateway.java
@@ -0,0 +1,147 @@
+package com.tutrit.httpclient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutrit.bean.Engineer;
+import com.tutrit.config.ConfigProvider;
+import com.tutrit.gateway.EngineerGateway;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+@Component
+public class HttpEngineerGateway implements EngineerGateway {
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String PATH = "engineers";
+    private final ObjectMapper objectMapper;
+    private final ConfigProvider config;
+    private final HttpClient httpClient;
+
+    public HttpEngineerGateway(
+            final ConfigProvider config,
+            final HttpClient httpClient,
+            final ObjectMapper objectMapper
+    ) {
+        this.objectMapper = objectMapper;
+        this.config = config;
+        this.httpClient = httpClient;
+    }
+    @Override
+    public Engineer saveEngineer(Engineer engineer) {
+        validateEngineer(engineer);
+
+        String url = "%s/%s/%s".formatted(config.getUrl(), PATH, engineer.engineerId());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", CONTENT_TYPE)
+                .POST(createEngineerBodyPublisher(engineer))
+                .build();
+
+        sendHttpRequest(request);
+        return engineer;
+    }
+
+    private void validateEngineer(Engineer engineer) {
+        if (engineer == null) {
+            throw new IllegalArgumentException("Engineer can't be null");
+        }
+
+        if (engineer.engineerId() == null || engineer.engineerId().isBlank()) {
+            throw new IllegalArgumentException("Engineer ID can't be null or empty");
+        }
+
+        if (engineer.firstName() == null || engineer.firstName().isBlank()) {
+            throw new IllegalArgumentException("Engineer first name can't be null or empty");
+        }
+
+        if (engineer.lastName() == null || engineer.lastName().isBlank()) {
+            throw new IllegalArgumentException("Engineer last name can't be null or empty");
+        }
+
+        if (engineer.function() == null || engineer.function().isBlank()) {
+            throw new IllegalArgumentException("Engineer function can't be null or empty");
+        }
+
+        if (engineer.category() == null || engineer.category().isBlank()) {
+            throw new IllegalArgumentException("Engineer category can't be null or empty");
+        }
+
+        if (engineer.education() == null || engineer.education().isBlank()) {
+            throw new IllegalArgumentException("Engineer education can't be null or empty");
+        }
+
+        if (engineer.experience() == null) {
+            throw new IllegalArgumentException("Engineer experience can't be null or empty");
+        }
+
+        if (engineer.generalExperience() == null) {
+            throw new IllegalArgumentException("Engineer generalExperience can't be null or empty");
+        }
+    }
+
+    private HttpRequest.BodyPublisher createEngineerBodyPublisher(Engineer engineer) {
+        try {
+            String string = objectMapper.writeValueAsString(engineer);
+            return HttpRequest.BodyPublishers.ofString(string);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error for serialize engineer object to JSON ", e);
+        }
+    }
+
+    @Override
+    public Optional<Engineer> findEngineerById(String engineerId) {
+        validateEngineerId(engineerId);
+
+        String url = "%s/%s/%s".formatted(config.getUrl(), PATH, engineerId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response = sendHttpRequest(request);
+        checkResponse(response);
+
+        Engineer engineer = mapJsonStringToEngineer(response);
+
+        return Optional.ofNullable(engineer);
+    }
+
+    private void validateEngineerId(String engineerId) {
+        if (engineerId == null || engineerId.isBlank()) {
+            throw new IllegalArgumentException("Engineer ID can't be null or empty");
+        }
+    }
+
+    private Engineer mapJsonStringToEngineer(HttpResponse<String> response) {
+        try {
+            return objectMapper.readValue(response.body(), Engineer.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Can't read mapJsonStringToEngineer", e);
+        }
+    }
+
+    private HttpResponse<String> sendHttpRequest(final HttpRequest request) {
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Can't read getStringHttpResponse", e);
+        }
+    }
+
+    private void checkResponse(HttpResponse<String> response) {
+        if (response.statusCode() < 200 || response.statusCode() > 299) {
+            throw new RuntimeException("Can't to save engineer: " + response.body());
+        }
+    }
+
+    @Override
+    public boolean deleteEngineerById(String engineerId) {
+        return false;
+    }
+}
