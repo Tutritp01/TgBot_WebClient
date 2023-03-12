a432d3ea20ac65127c458396101a1f48a34d1474
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-03-12 15:27:29 +0300

    revert main class into web-iu

--- web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
+++ web-ui/src/main/java/com/tutrit/webclient/WebClientApplication.java
@@ -1,17 +1,14 @@
-package com.tutrit.webClientRun;
+package com.tutrit.webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages  ="com.tutrit")
-public class ApplicationRun {
+public class WebClientApplication {
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(ApplicationRun.class, args);
-        System.out.println(ctx);
+        SpringApplication.run(WebClientApplication.class, args);
     }
-
 }
-
