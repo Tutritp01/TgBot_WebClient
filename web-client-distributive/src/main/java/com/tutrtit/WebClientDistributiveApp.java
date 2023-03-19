c1c3c5f06b8240a15f9ae6aea4a53d53f80edb57
mikas <6666350@gmail.com>
2023-03-19 13:31:44 +0100

    adds connection to configuration server

--- web-client-distributive/src/main/java/com/tutrtit/WebClientDistributiveApp.java
+++ web-client-distributive/src/main/java/com/tutrtit/WebClientDistributiveApp.java
@@ -2,14 +2,13 @@ package com.tutrtit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages  ="com.tutrit")
-public class ApplicationRun {
+public class WebClientDistributiveApp {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRun.class, args);
+        SpringApplication.run(WebClientDistributiveApp.class, args);
     }
 }
 
