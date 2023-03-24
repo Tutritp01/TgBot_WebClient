0a2169d26c410fda3ee86dd328d86a295c8e7e20
“Artem <3947710@gmail.com>
2023-03-24 11:13:20 +0300

    [TGBOT-313] Remove InfoController and info.html

--- web-ui/src/main/java/com/tutrit/webclient/controller/HomePageController.java
+++ web-ui/src/main/java/com/tutrit/webclient/controller/HomePageController.java
@@ -1,15 +1,29 @@
 package com.tutrit.webclient.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Controller
 public class HomePageController {
     @GetMapping("/")
-    public ModelAndView openHomePage() {
+    public ModelAndView openHomePage() throws IOException, InterruptedException {
+        String url = "http://localhost:8081/actuator/info";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         var mov = new ModelAndView();
         mov.setViewName("starter");
+        mov.addObject("infoResponse", response.body());
         return mov;
 
     }
